0f1ad457e3328f53bd18581c8c37830459f9c556
CollinK23 <kimballcollin@gmail.com>
2024-03-14 03:22:27 -0700

    Created tests for clearing chart data

--- src/chartBuilder/chartBuilder.test.js
+++ src/chartBuilder/chartBuilder.test.js
@@ -128,3 +128,104 @@ test("Generating a chart without entering x and y values alerts the user ", asyn
 
   await user.click(clear);
 });
+
+test("Clicking the clear chart button resets all x and y values", async () => {
+  initDomFromFiles(
+    `${__dirname}/../line/line.html`,
+    `${__dirname}/../line/line.js`
+  );
+
+  let xInputs = domTesting.getAllByLabelText(document, "X");
+  let yInputs = domTesting.getAllByLabelText(document, "Y");
+
+  const addCoords = domTesting.getByText(document, "+");
+
+  const user = userEvent.setup();
+
+  await user.type(xInputs[xInputs.length - 1], "7");
+  await user.type(yInputs[yInputs.length - 1], "9");
+
+  await user.click(addCoords);
+
+  await user.type(xInputs[xInputs.length - 1], "7");
+  await user.type(yInputs[yInputs.length - 1], "9");
+
+  xInputs = domTesting.getAllByLabelText(document, "X");
+  yInputs = domTesting.getAllByLabelText(document, "Y");
+
+  expect(xInputs.length).toBe(2);
+  expect(yInputs.length).toBe(2);
+
+  let clear = domTesting.getByText(document, "Clear chart data");
+
+  await user.click(clear);
+
+  xInputs = domTesting.getAllByLabelText(document, "X");
+  yInputs = domTesting.getAllByLabelText(document, "Y");
+
+  expect(xInputs.length).toBe(1);
+  expect(yInputs.length).toBe(1);
+
+  expect(xInputs[xInputs.length - 1]).toHaveValue(null);
+  expect(yInputs[yInputs.length - 1]).toHaveValue(null);
+});
+
+test("Clicking the clear chart button resets x and y labels", async () => {
+  initDomFromFiles(
+    `${__dirname}/../line/line.html`,
+    `${__dirname}/../line/line.js`
+  );
+
+  let xInput = domTesting.getByLabelText(document, "X label");
+  let yInput = domTesting.getByLabelText(document, "Y label");
+
+  const user = userEvent.setup();
+
+  await user.type(xInput, "Cats");
+  await user.type(yInput, "Dogs");
+
+  let clear = domTesting.getByText(document, "Clear chart data");
+
+  await user.click(clear);
+
+  expect(xInput).toHaveValue("");
+  expect(yInput).toHaveValue("");
+});
+
+test("Clicking the clear chart button resets chart title", async () => {
+  initDomFromFiles(
+    `${__dirname}/../line/line.html`,
+    `${__dirname}/../line/line.js`
+  );
+
+  let chartTitle = domTesting.getByLabelText(document, "Chart title");
+
+  const user = userEvent.setup();
+
+  await user.type(chartTitle, "Cats");
+
+  let clear = domTesting.getByText(document, "Clear chart data");
+
+  await user.click(clear);
+
+  expect(chartTitle).toHaveValue("");
+});
+
+test("Clicking the clear chart button resets chart color", async () => {
+  initDomFromFiles(
+    `${__dirname}/../line/line.html`,
+    `${__dirname}/../line/line.js`
+  );
+
+  let chartColor = domTesting.getByLabelText(document, "Chart color");
+
+  const user = userEvent.setup();
+
+  userEvent.type(chartColor, "#FFFFFF");
+
+  let clear = domTesting.getByText(document, "Clear chart data");
+
+  await user.click(clear);
+
+  expect(chartColor.value).toBe("#ff4500");
+});
